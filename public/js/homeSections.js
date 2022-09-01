b84a89b3a8af2f5e0e78ddc59cb8d5eaf2af72dc
Grong <lionel.de-sousa@epitech.eu>
2022-09-01 17:01:56 +0200

    [UPD] shop responsive [FIX] section background

--- public/js/homeSections.js
+++ public/js/homeSections.js
@@ -1,6 +1,7 @@
 let section = document.querySelectorAll('#section');
 let sectionText = document.querySelectorAll('#section-text');
 let nb = $("div[id^=category]").length;
+let paid = document.querySelectorAll("#card > div > a > img");
 
 for (let i = 0; i < nb; i++) {
     if (i % 2 === 0)
@@ -10,3 +11,12 @@ for (let i = 0; i < nb; i++) {
         sectionText[i].style.color = "#FFFFFF";
     }
 }
+
+paid[14].classList.add("paid-port");
+paid[18].classList.add("paid");
+paid[19].classList.add("paid");
+paid[20].classList.add("paid");
+paid[20].classList.add("paid-spe");
+paid[21].classList.add("paid");
+paid[22].classList.add("paid");
+paid[23].classList.add("paid");
