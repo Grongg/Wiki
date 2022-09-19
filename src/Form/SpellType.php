f86e18949417daa3f37e6b5d176c073d47a96aef
Grong <lionel.de-sousa@epitech.eu>
2022-09-19 14:25:36 +0200

    [UPD] spell basic crud [ADD] footer admin

--- src/Form/SpellType.php
+++ src/Form/SpellType.php
@@ -9,6 +9,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class SpellType extends AbstractType
 {
@@ -17,18 +19,61 @@ class SpellType extends AbstractType
         $builder
             ->add('name', TextType::class,[
                 'required' => false,
-                'label' => 'Nom du produit',
+                'label' => 'Nom du spell',
                 'attr' => [
                     'placeholder' => 'Taper le nom ici...'
                 ]
             ])
-            ->add('isActive')
-            ->add('isPassive')
+            ->add('spellId', TextType::class,[
+                'required' => false,
+                'label' => 'id du spell',
+                'attr' => [
+                    'placeholder' => 'Taper le nom ici...'
+                ]
+            ])
+            ->add('isActive', CheckboxType::class,[
+                'required' => false,
+                'label' => 'si sort actif',
+                'attr' => [
+                    'placeholder' => 'Taper le nom ici...'
+                ]
+            ])
+            ->add('isPassive', CheckboxType::class,[
+                'required' => false,
+                'label' => 'si sort passif',
+                'attr' => [
+                    'placeholder' => 'Taper le nom ici...'
+                ]
+            ])
+            ->add('isToggle', CheckboxType::class,[
+                'required' => false,
+                'label' => 'si sort toggle',
+                'attr' => [
+                    'placeholder' => 'Taper le nom ici...'
+                ]
+            ])
+            ->add('isBoth', CheckboxType::class,[
+                'required' => false,
+                'label' => 'si sort actif et passif',
+                'attr' => [
+                    'placeholder' => 'Taper le nom ici...'
+                ]
+            ])     
+            ->add('isChampPassive', CheckboxType::class,[
+                'required' => false,
+                'label' => 'si sort passif du champion',
+                'attr' => [
+                    'placeholder' => 'Taper le nom ici...'
+                ]
+            ])       
             ->add('resume')
             ->add('spellImage')
-            ->add('cost')
+            // ->add('cost')
             ->add('type')
-            ->add('cooldown')
+            // ->add('cooldown', CollectionType::class, [
+            //     'required' => false,
+            //     'label' => 'couts du sort',
+            //     ])
             ->add('champion',EntityType::class,[
                 'required' => false,
                 'label' => 'Champion du spell',
